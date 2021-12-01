4184b4d10fe89df46ebf3c2e1fa162d815c3f484
atu4403 <73111778+atu4403@users.noreply.github.com>
2021-12-01 16:30:22 +0900

    🆕 feat: README自動作成スクリプトを追加

---
+++ make_readme.fish
@@ -0,0 +1,26 @@
+function make_readme
+    set til_dir /Users/atu/ghq/github.com/atu4403/til
+    cd $til_dir
+    echo "# til
+
+til - Today I learned.
+
+## 環境
+
+macos
+
+## contents"
+    for child in (ls)
+        if test -d $child
+            echo -e "\n### $child\n"
+            for file in (ls $child)
+                if test (echo $file| sed 's/^.*\.\([^\.]*\)$/\1/') = md
+                    set txt (cat $til_dir/$child/$file)
+                    set title (echo (string replace '# ' '' $txt[1]))
+                    echo '- '\[$title\]\($child/$file\)
+                end
+            end
+        end
+    end
+end
+make_readme >README.md
