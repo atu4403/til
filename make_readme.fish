97ca9523163f6fdf71b18349355572b6ff1b83ef
atu4403 <73111778+atu4403@users.noreply.github.com>
2021-12-02 21:19:40 +0900

    👻 fix: imagesと'# WIP'で始まる記事をREADMEに含めないように修正

--- make_readme.fish
+++ make_readme.fish
@@ -12,11 +12,17 @@ macos
 ## contents"
     for child in (ls)
         if test -d $child
+            if test $child = images
+                continue
+            end
             echo -e "\n### $child\n"
             for file in (ls $child)
                 if test (echo $file| sed 's/^.*\.\([^\.]*\)$/\1/') = md
                     set txt (cat $til_dir/$child/$file)
                     set title (echo (string replace '# ' '' $txt[1]))
+                    if test $title = WIP
+                        continue
+                    end
                     echo '- '\[$title\]\($child/$file\)
                 end
             end
